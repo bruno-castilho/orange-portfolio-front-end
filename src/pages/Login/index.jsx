ece517ba5f102d54730c8a6d079cd5f7754487ad
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2024-01-24 15:42:25 -0300

    add pagina login
    criação da pagina login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,68 @@
 import styles from './styles.module.css'
+import logo from '../../assets/logo googleg 48dp.svg'
+import frame from '../../assets/Frame.svg'
+import TextField from '@mui/material/TextField';
+import CustomizedButtons from './CustomizedButtons'; 
+
 
 export function Login() {
-  return <h1>Login</h1>
+  return (
+    <main className={styles.main}>
+      <div className={styles.imagem_login}></div>
+      <div className={styles.layout_form}>
+        <h3>Entre no Orange Portfólio</h3>
+        <a className={styles.btn_google} href="">
+          <img src={logo} alt="" />
+          <p>Entrar com Google</p>
+        </a>
+        <div className={styles.form_contaniner}>
+          <div className={styles.box_login}>
+            <h5>Faça login com email</h5>
+            <img src={frame} alt="" />
+          </div>
+          <form action="" method="post">
+            <div className={styles.email_password}>
+              <TextField  className={styles.input}
+                type="email"
+                id="email"
+                label="Email"
+                variant="outlined"
+                InputLabelProps={{
+                  style: { color: '#000000' }, 
+                }}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderColor: '#000000',
+                    "&.Mui-focused fieldset": {
+                      borderColor: '#000000',
+                    },
+                  },
+                }}
+              />
+              <TextField className={styles.input} 
+              type="password" 
+              id="password" 
+              label="password" 
+              variant="outlined"  InputLabelProps={{
+                style: { color: '#000000' }, 
+              }}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  borderColor: '#000000',
+                  "&.Mui-focused fieldset": {
+                    borderColor: '#000000',
+                  },
+                },
+              }}/>
+            </div>
+            <CustomizedButtons type="submit" className={styles.button}/>
+          </form>
+          
+          <div className={styles.teste}>
+          <a href="">Cadastre-se</a>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
