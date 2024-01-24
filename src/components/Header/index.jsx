53f0a3c692cb0d7ea9b3a20d8dda0922e7c0a064
Bruno <bruno.silva.castilho@gmail.com>
2024-01-24 14:04:53 -0300

    finalizado header responsivo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,31 +1,11 @@
-import IconButton from '@mui/material/IconButton'
-
-import styles from './styles.module.css'
-import logo from '../../assets/logo.svg'
-import perfil from '../../assets/perfil.svg'
-import { Notifications } from '@mui/icons-material'
-import { Link } from 'react-router-dom'
+import { Desktop } from './desktop'
+import { Mobile } from './mobile'
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.menu}>
-          <img src={logo} alt="" />
-          <div>
-            <div>
-              <Link to="/meusprojetos">Meus projetos</Link>
-              <Link to="/descobrir">Descobrir</Link>
-            </div>
-          </div>
-        </div>
-        <div className={styles.userContent}>
-          <img src={perfil} alt="" />
-          <IconButton color="inherit" size="medium">
-            <Notifications />
-          </IconButton>
-        </div>
-      </div>
-    </header>
+    <>
+      <Mobile />
+      <Desktop />
+    </>
   )
 }
---
+++ src/components/Header/mobile/index.jsx
@@ -0,0 +1,175 @@
+import * as React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import styles from './styles.module.css'
+import logo from '../../../assets/logo.svg'
+import perfil from '../../../assets/perfil.svg'
+import { Notifications } from '@mui/icons-material'
+import Paper from '@mui/material/Paper'
+import {
+  Divider,
+  Grow,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  MenuItem,
+  MenuList,
+} from '@mui/material'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { Link } from 'react-router-dom'
+import Popper from '@mui/material/Popper'
+
+export function Mobile() {
+  const [open, setOpen] = React.useState(false)
+  const anchorRef = React.useRef(null)
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen)
+  }
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return
+    }
+
+    setOpen(false)
+  }
+
+  const prevOpen = React.useRef(open)
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus()
+    }
+
+    prevOpen.current = open
+  }, [open])
+
+  return (
+    <div className={styles.container}>
+      <Box>
+        <AppBar
+          position="static"
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flexStart',
+            background: 'var(--color-principal-100)',
+          }}
+        >
+          <Toolbar
+            sx={{
+              display: 'flex',
+              padding: '0px var(--3, 24px)',
+              alignItems: 'center',
+              alignSelf: 'stretch',
+              borderRadius: '4px',
+              height: '64px',
+            }}
+          >
+            <IconButton
+              color="inherit"
+              size="medium"
+              ref={anchorRef}
+              onClick={handleToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <img src={logo} alt="" className={styles.logo} />
+            </Typography>
+
+            <div className={styles.userContent}>
+              <img src={perfil} alt="" />
+              <IconButton color="inherit" size="medium">
+                <Notifications />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === 'bottom-start' ? 'left top' : 'left bottom',
+              }}
+            >
+              <Paper
+                variant="elevation"
+                elevation={10}
+                square={false}
+                sx={{ width: '220px', marginTop: '8px' }}
+              >
+                <List>
+                  <ListItem>
+                    <ListItemText
+                      primary={
+                        <Typography
+                          variant="body1"
+                          sx={{
+                            color: 'var(--text-primary), rgba(0, 0, 0, 0.87)',
+                          }}
+                        >
+                          Camila
+                        </Typography>
+                      }
+                      secondary={
+                        <Typography
+                          variant="body2"
+                          sx={{
+                            color: 'rgba(0, 0, 0, 0.60)',
+                            fontFeatureSettings: 'clig off, liga off',
+                          }}
+                        >
+                          Camila.ux@gmail.com
+                        </Typography>
+                      }
+                    />
+                  </ListItem>
+                </List>
+                <Divider />
+                <MenuList>
+                  <MenuItem
+                    component={Link}
+                    to="/meusprojetos"
+                    onClick={handleClose}
+                  >
+                    Meus Projetos
+                  </MenuItem>
+
+                  <MenuItem
+                    component={Link}
+                    to="/descobrir"
+                    onClick={handleClose}
+                  >
+                    Descobrir
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem onClick={handleClose}>
+                    <ListItemIcon>
+                      <LogoutIcon fontSize="medium" />
+                    </ListItemIcon>
+                    <Typography variant="inherit">Sair</Typography>
+                  </MenuItem>
+                </MenuList>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </Box>
+    </div>
+  )
+}
