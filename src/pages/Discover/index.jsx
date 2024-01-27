d83644ffe8e741538501265adced4107215a6906
lucassantosdahora <116045229+lucassantosdahora@users.noreply.github.com>
2024-01-26 22:45:29 -0300

    add pagina descobrir(falta os cards)

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,5 +1,34 @@
-import styles from './styles.module.css'
-
+import styles from './styles.module.css';
+import { TextField } from '@mui/material';
+import {ModalComponent} from './modal'
 export function Discover() {
-  return <h1>Descobrir</h1>
+
+  return (
+    <main>
+      <div className={styles.title_container}>
+        <h4 className={styles.title}>Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</h4>
+      </div>
+
+      <div className={styles.input_container}>
+        <TextField
+          fullWidth
+          label="Buscar Tags"
+          variant="outlined"
+          size="medium"
+          InputLabelProps={{
+            style: { color: '#000000' },
+          }}
+          sx={{
+            "& .MuiOutlinedInput-root": {
+              borderColor: '#000000',
+              "&.Mui-focused fieldset": {
+                borderColor: '#000000',
+              },
+            },
+          }}
+        />
+      </div>
+     < ModalComponent />
+    </main>
+  );
 }
