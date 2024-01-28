c82db49bd0c2dbf3bc862108ea36333cced217aa
Bruno <bruno.silva.castilho@gmail.com>
2024-01-27 21:05:18 -0300

    Adicionado styled-components na tela de login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,16 +1,24 @@
-import styles from './styles.module.css'
 import logo from '../../assets/logo_googleg_48dp.svg'
-import frame from '../../assets/Frame.svg'
 
-import CustomizedButtons from './CustomizedButtons'
+import imgLogin from '../../assets/img_login.svg'
+
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Link } from 'react-router-dom'
-import { TextField } from '@mui/material'
+import { Button, TextField } from '@mui/material'
 import { useContext } from 'react'
 import { AuthContext } from '../../contexts/AuthContext'
 
 import * as zod from 'zod'
+import {
+  ButtonContainer,
+  LoginContainer,
+  LoginContent,
+  LoginForm,
+  LoginFormContainer,
+  LoginImg,
+  LoginSection,
+} from './styles'
 
 const LoginValidationSchema = zod.object({
   email: zod.string().email({ message: 'Digite um e-mail válido' }),
@@ -20,7 +28,7 @@ const LoginValidationSchema = zod.object({
 export function Login() {
   const { handleGoogleLogin, handleLogin } = useContext(AuthContext)
 
-  const LoginForm = useForm({
+  const LoginFormData = useForm({
     resolver: zodResolver(LoginValidationSchema),
     defaultValues: {
       email: '',
@@ -28,75 +36,86 @@ export function Login() {
     },
   })
 
-  const { handleSubmit, register } = LoginForm
+  const { handleSubmit, register } = LoginFormData
 
   return (
-    <main className={styles.main}>
-      <div className={styles.imagem_login}></div>
-      <div className={styles.layout_form}>
-        <h3 className={styles.title}>Entre no Orange Portfólio</h3>
-        <button
-          type="button"
-          className={styles.btn_google}
-          onClick={handleGoogleLogin}
-        >
-          <img src={logo} alt="" />
-          <p>Entrar com Google</p>
-        </button>
-        <div className={styles.form_contaniner}>
-          <div className={styles.box_login}>
-            <h5 className={styles.subtitle}>Faça login com email</h5>
-            <img src={frame} alt="" />
-          </div>
-          <form action="" onSubmit={handleSubmit(handleLogin)}>
-            <div className={styles.email_password}>
-              <TextField
-                className={styles.input}
-                type="email"
-                id="email"
-                label="Email"
-                variant="outlined"
-                InputLabelProps={{
-                  style: { color: '#000000' },
-                }}
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    borderColor: '#000000',
-                    '&.Mui-focused fieldset': {
-                      borderColor: '#000000',
-                    },
-                  },
-                }}
-                {...register('email')}
-              />
-              <TextField
-                className={styles.input}
-                type="password"
-                id="password"
-                label="password"
-                variant="outlined"
-                InputLabelProps={{
-                  style: { color: '#000000' },
-                }}
-                sx={{
-                  '& .MuiOutlinedInput-root': {
-                    borderColor: '#000000',
-                    '&.Mui-focused fieldset': {
-                      borderColor: '#000000',
-                    },
-                  },
-                }}
-                {...register('password')}
-              />
-            </div>
-            <CustomizedButtons type="submit" className={styles.button} />
-          </form>
+    <LoginContainer>
+      <LoginContent>
+        <LoginImg src={imgLogin} />
+        <LoginSection>
+          <div>
+            <h3>Entre no Orange Portfólio</h3>
+            <ButtonContainer>
+              <button type="button" onClick={handleGoogleLogin}>
+                <img src={logo} alt="" />
+                <p>Entrar com Google</p>
+              </button>
+            </ButtonContainer>
 
-          <div className={styles.registro}>
-            <Link to={'/registrar'}>Cadastre-se</Link>
+            <LoginFormContainer>
+              <LoginForm onSubmit={handleSubmit(handleLogin)}>
+                <h5>Faça login com email</h5>
+
+                <TextField
+                  type="email"
+                  label="Email address"
+                  variant="outlined"
+                  InputLabelProps={{
+                    style: { color: '#000000' },
+                  }}
+                  sx={{
+                    width: '100%',
+                    fontFamily: 'Roboto',
+                    '& .MuiOutlinedInput-root': {
+                      borderColor: '#000000',
+                      '&.Mui-focused fieldset': {
+                        borderColor: '#000000',
+                      },
+                    },
+                  }}
+                  {...register('email')}
+                />
+                <TextField
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  InputLabelProps={{
+                    style: { color: '#000000' },
+                  }}
+                  sx={{
+                    width: '100%',
+                    fontFamily: 'Roboto',
+                    '& .MuiOutlinedInput-root': {
+                      borderColor: '#000000',
+                      '&.Mui-focused fieldset': {
+                        borderColor: '#000000',
+                      },
+                    },
+                  }}
+                  {...register('password')}
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  ele
+                  sx={{
+                    width: '100%',
+                    background: 'var(--color-secondary-100)',
+                    '&:hover': {
+                      background: 'var(--color-secondary-110)',
+                    },
+                  }}
+                >
+                  Entrar
+                </Button>
+
+                <Link to={'/registrar'}>Cadastre-se</Link>
+              </LoginForm>
+            </LoginFormContainer>
           </div>
-        </div>
-      </div>
-    </main>
+        </LoginSection>
+      </LoginContent>
+    </LoginContainer>
   )
 }
