e7a3ac085a7e69d756e450c9351630b9f1a73f15
Bruno <bruno.silva.castilho@gmail.com>
2024-01-31 14:42:02 -0300

    Feito intregração da pagian discover com o back-end

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,34 +1,62 @@
-import styles from './styles.module.css';
-import { TextField } from '@mui/material';
-import { ModalComponent } from '../../components/Modals/cardView/view'; 
+import styles from './styles.module.css'
+import { TextField } from '@mui/material'
+import { ModalComponent } from '../../components/Modals/cardView/view'
+import { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import api from '../../confs/api'
+import { Filter } from '../../components/Filter'
+import {
+  ProjectList,
+  ProjectFilter,
+  DiscoverContainer,
+  DiscoverHeader,
+} from './styles'
+import { CardProject } from '../../components/ProjectCard'
 
 export function Discover() {
-  return (
-    <main>
-      <div className={styles.title_container}>
-        <h4 className={styles.title}>Junte-se à comunidade de inovação, inspiração e descobertas, transformando experiências em conexões inesquecíveis</h4>
-      </div>
+  const { token } = useContext(AuthContext)
+  const [projects, setProjects] = useState([])
+  const [projectsFiltered, setProjectsFiltered] = useState([])
 
-      <div className={styles.input_container}>
-        <TextField
-          fullWidth
-          label="Buscar Tags"
-          variant="outlined"
-          size="medium"
-          InputLabelProps={{
-            style: { color: '#000000' },
-          }}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              borderColor: '#000000',
-              "&.Mui-focused fieldset": {
-                borderColor: '#000000',
-              },
-            },
-          }}
-        />
-      </div>
+  useEffect(() => {
+    const params = { token }
+    api
+      .get('/projetos', { params })
+      .then((response) => {
+        setProjects(response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [token])
+
+  function datReturnFunc(data) {
+    setProjectsFiltered(data)
+  }
+
+  return (
+    <DiscoverContainer>
+      <DiscoverHeader>
+        <h5>
+          Junte-se à comunidade de inovação, inspiração e descobertas,
+          transformando experiências em conexões inesquecíveis
+        </h5>
+      </DiscoverHeader>
+
+      <section>
+        <ProjectFilter>
+          <div>
+            <Filter data={projects} datReturnFunc={datReturnFunc} />
+          </div>
+        </ProjectFilter>
+
+        <ProjectList>
+          {projectsFiltered?.map((project) => (
+            <CardProject key={project.id} project={project} />
+          ))}
+        </ProjectList>
+      </section>
       <ModalComponent />
-    </main>
-  );
+    </DiscoverContainer>
+  )
 }
